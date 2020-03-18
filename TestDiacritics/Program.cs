fc9339e0ee409152b7c7e8781995891d5d219b01
jorge.carrazco@medicinasrosario.com.mx <jorge.carrazco@medicinasrosario.com.mx>
2020-03-18 10:24:54 -0600

    Agregar archivos de proyecto.

---
+++ TestDiacritics/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDiacritics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tecle una letra");
+            string texto = Console.ReadLine().ToString();
+
+            if (CheckForDiacritics(texto))
+            {
+                Console.WriteLine("Es diacritico");
+            }
+            else
+            {
+                Console.WriteLine("No es diacritico");
+            }
+            Console.ReadKey();
+        }
+
+        public static bool CheckForDiacritics(string text)
+        {
+            string[] character = { "á", "é", "í", "ó", "ú", "ñ" };
+
+            for (int count = 0; count < character.Length; count++)
+            {
+                if(text.IndexOf(character[count], StringComparison.CurrentCultureIgnoreCase) > -1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
